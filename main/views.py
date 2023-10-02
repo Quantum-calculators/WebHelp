e3bc0e6c74d05947908e6e09625956ce2a2b0e6b
Alexandr Fonov <FonovAD@Gmail.com>
2023-10-02 18:45:35 +0300

    Add constants with error messages
    Error messages have been rendered to constants

--- main/views.py
+++ main/views.py
@@ -6,17 +6,22 @@ from .parser import Parser
 from .Classifier.process import topic_determinant
 
 
+ERROR_MESSAGE = "Could not access the resource. "
+ERROR_CODE = "Status code of the specified site: "
+URL_ERROR = "The url was not specified"
+PARSER_ERROR = "Parse Error"
+
+
 class MainView(View):
     template_name = "main/index.html"
 
     def get(self, request):
         form = LinkForm()
-        return render(request, "main/index.html", {"form": form})
+        return render(request, self.template_name, {"form": form})
 
     def post(self, request):
         data = request.POST
         form = LinkForm()
-        # if request.is_ajax():
         if data["link"]:
             link = data["link"]
             parsed_data = Parser.parse(link)
@@ -25,13 +30,10 @@ class MainView(View):
             )
             if theme[0] != 0:
                 if theme[0] != 1:
-                    category_name = (
-                        "Could not access the resource. Status code of the specified site: "
-                        + str(theme[0])
-                    )
+                    category_name = ERROR_MESSAGE + ERROR_CODE + str(theme[0])
                     theme_name = ""
                 else:
-                    category_name = "Could not access the resource."
+                    category_name = ERROR_MESSAGE
                     theme_name = ""
             else:
                 category_name = theme[2]
@@ -39,7 +41,7 @@ class MainView(View):
             response = {"category": category_name, "theme": theme_name, "link": link}
             return JsonResponse(response, status=200)
         else:
-            errors = "The url was not specified"
+            errors = URL_ERROR
             return JsonResponse({"errors": errors}, status=400)
 
 
@@ -49,17 +51,13 @@ def check_domain(request):
     theme = topic_determinant(list_of_words=parsed_data[1], exit_code=parsed_data[2])
     if theme[0] != 0:
         if theme[0] != 1:
-            category_name = (
-                "Could not access the resource. Status code of the specified site: "
-                + str(theme[0])
-            )
-            theme_name = "Parse Error"
+            category_name = ERROR_MESSAGE + str(theme[0])
+            theme_name = PARSER_ERROR
         else:
-            category_name = "Could not access the resource."
+            category_name = ERROR_MESSAGE
             theme_name = f"Status code: {theme[0]}"
     else:
         category_name = str(theme[1])
         theme_name = str(theme[1])
     response = {"category": category_name, "theme": theme_name, "link": link}
     return JsonResponse(response)
-    # return JsonResponse(response, json_dumps_params={'ensure_ascii': False})
